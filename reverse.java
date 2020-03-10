e62906cc9a476a6b4741f1ec773e026d2e783480
Anji399 <58341395+Anji399@users.noreply.github.com>
2020-03-10 10:13:00 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -2,7 +2,7 @@ public class ReverseNumber {
 
     public static void main(String[] args) {
 
-        int num = 1234, reversed = 0;
+        int num = 123, reversed = 0;
 
         while(num != 0) {
             int digit = num % 10;
